451dac7303469776c25153788777f96d35000145
ihza <ihzan0@gmail.com>
2022-10-03 10:57:39 +0700

    deploy

--- server/orchestrator/config/index.js
+++ server/orchestrator/config/index.js
@@ -1,5 +1,5 @@
-const baseUrlUsers = "http://localhost:4001";
-const baseUrlApp = "http://localhost:4002";
+const baseUrlUsers = "https://p3-services-users.herokuapp.com";
+const baseUrlApp = "https://p3-services-app.herokuapp.com";
 const Redis = require("ioredis");
 const redis = new Redis(
   "redis://default:HkeVOvqi59nDxmw4OxI9XK4JvZfXu9mo@redis-12392.c252.ap-southeast-1-1.ec2.cloud.redislabs.com:12392/0"
